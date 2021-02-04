91b8a07f7f39ce1680f17c4a44a661c175881bde
Dale Ma <dma@opengov.com>
2021-02-04 06:03:02 -0800

    updated config

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "okm0sled99uqmojct1tu0q6fu",     // CognitoClientID
-  "api_base_url": "https://jklt4o9qrd.execute-api.us-west-2.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "aws_user_pools_web_client_id": "b49b867o8q2va4e0tqjkkclp9",     // CognitoClientID
+  "api_base_url": "https://id43frthx4.execute-api.us-west-2.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-todoro.auth.us-west-2.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d1mhxre6fjxb1e.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://master.d1s192d12vlwjg.amplifyapp.com"                                      // AmplifyURL
   // "redirect_url": "https://localhost:8080"
 };
 
