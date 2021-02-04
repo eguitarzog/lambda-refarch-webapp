9f6050e4b054b82e904e11debebc6966472faf2b
Dale Ma <dma@opengov.com>
2021-02-04 05:03:10 -0800

    Update frontend config

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,11 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "okm0sled99uqmojct1tu0q6fu",     // CognitoClientID
+  "api_base_url": "https://jklt4o9qrd.execute-api.us-west-2.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-todoro.auth.us-west-2.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1mhxre6fjxb1e.amplifyapp.com"                                      // AmplifyURL
+  // "redirect_url": "https://localhost:8080"
 };
 
 export default config;
